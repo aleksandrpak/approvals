d36441e43eaafb4bc1786561cf9f1f3a3179dda7
Aleksandr Pak <aleksandr.pak@mendix.com>
2016-02-25 22:51:59 +0100

    draft version of approve

--- src/get_method_name.rs
+++ src/get_method_name.rs
@@ -3,6 +3,7 @@ extern crate backtrace;
 use std::fmt;
 use std::str;
 
+#[derive(Debug)]
 pub enum Error {
     InvalidLevel,
     NoSymbolName,
@@ -23,14 +24,14 @@ impl From<fmt::Error> for Error {
     }
 }
 
-pub fn get_method_name() -> Result<String, Error> {
+pub fn get_method_name(levels_up: usize) -> Result<String, Error> {
     let mut result = Err(Error::InvalidLevel);
     let mut level = 0;
 
     backtrace::trace(&mut |frame| {
         let ip = frame.ip();
 
-        backtrace::resolve(ip, &mut |symbol| result = resolve_method(symbol, level));
+        backtrace::resolve(ip, &mut |s| result = resolve_method(s, level, levels_up));
 
         level = level + 1;
 
@@ -43,12 +44,15 @@ pub fn get_method_name() -> Result<String, Error> {
     result
 }
 
-fn resolve_method(symbol: &backtrace::Symbol, level: usize) -> Result<String, Error> {
+fn resolve_method(symbol: &backtrace::Symbol,
+                  level: usize,
+                  levels_up: usize)
+                  -> Result<String, Error> {
     // 0 - backtrace::trace
     // 1 - get_method_name
-    // 2 - approve
-    // 3 - your_method_name
-    if level < 3 {
+    // 2 - calling_method => levels_up = 0
+    // 3 - method_name_1 => levels_up = 1
+    if level < 2 + levels_up {
         return Err(Error::InvalidLevel);
     }
 
